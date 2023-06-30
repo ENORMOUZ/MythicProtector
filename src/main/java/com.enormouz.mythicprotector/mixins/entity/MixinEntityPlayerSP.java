eea4cdf9a48f4402b42c0e5003d2c3d74532e52a
ENORMOUZ <zxliem0@gmail.com>
2023-06-30 23:39:19 +0800

    Remove unnecessary mixins

--- src/main/java/com.enormouz.mythicprotector/mixins/entity/MixinEntityPlayerSP.java
+++ src/main/java/com.enormouz.mythicprotector/mixins/entity/MixinEntityPlayerSP.java
@@ -1,16 +1,20 @@
 package com.enormouz.mythicprotector.mixins.entity;
 
 import com.enormouz.mythicprotector.utils.ChestUtils;
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = {EntityPlayerSP.class}, priority = 1)
-public abstract class MixinEntityPlayerSP extends MixinAbstractClientPlayer {
+public abstract class MixinEntityPlayerSP extends AbstractClientPlayer {
 
-    public MixinEntityPlayerSP() {
+    public MixinEntityPlayerSP(World worldIn, GameProfile playerProfile) {
+        super(worldIn, playerProfile);
     }
 
     @Inject(method = "closeScreen", at = @At("HEAD"), cancellable = true)
